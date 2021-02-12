49813e9fbfef30f34c666ae2fbd81fab56b4269b
Dannie Gregoire <dannie@dgregoire.com>
2021-02-11 19:41:33 -0500

    Added search endpoint stub

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const app = express();
 
 const PORT = 3080;
 
-
+app.get('/search', (req, res) => {
+  res.send('Search endpoint.')
+})
 app.listen(PORT, () => {
   console.log(`Movie Finder API Server started on port ${PORT}`);
 })
