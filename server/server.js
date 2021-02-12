63cab929fc393e8cdb28839f290b91d02737ad05
Dannie Gregoire <dannie@dgregoire.com>
2021-02-11 21:41:18 -0500

    Basic search endpoint is operational

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
-const express = require('express');
+/*
+ *  Movie Finder API Server 
+ */
 
+const express = require('express');
+const axios = require('axios');
 const app = express();
 
-const PORT = 3080;
+// Environment Variables
+const PORT = process.env.MFA_PORT || 3080;
+const OMDB_KEY = process.env.MFA_OMDB_KEY;
+const OMDB_URL = 'https://www.omdbapi.com';
 
-app.get('/search', (req, res) => {
-  res.send('Search endpoint.')
-})
+app.get("/search",async (req, res) => {
+  try{
+    let movieResult = {};
+    let title = encodeURIComponent(req.query.title);
+
+    if(title){
+      resp = await axios.get(`${OMDB_URL}/?s=${title}&apikey=${OMDB_KEY}`);
+      movieResult = resp.data.Search[0] || '{}';
+    }
+    console.log(`Searched for "${title}" - found "${movieResult.Title}"`);
+    res.send(movieResult);
+  }
+  catch(err){
+    res.send({});
+    console.warn(`Problem fetching results`)
+    console.warn(err);
+  }
+});
 app.listen(PORT, () => {
   console.log(`Movie Finder API Server started on port ${PORT}`);
 })
