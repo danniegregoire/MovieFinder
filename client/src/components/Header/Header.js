336ea336e2cd681c5765e158a85ac88d0394b6f0
Dannie Gregoire <dannie@dgregoire.com>
2021-02-14 12:03:00 -0500

    Added fontawsome film icon to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header = () => {
   return (
-    <div className="app-header"><h1>Movie Finder</h1></div>
+    <div className="app-header">
+      <FontAwesomeIcon icon={faFilm} className="fas fa-film" size="2x"/>
+      <h1>Movie Finder</h1>
+    </div>
   )
 }
 
