6da94ad5a6aaa85db5d6d19341c951ae0bd662d6
Dannie Gregoire <dannie@dgregoire.com>
2021-02-13 20:28:34 -0500

    updated api with ability to fetch inidividual movie details by imdb id and title

--- server/server.js
+++ server/server.js
@@ -21,24 +21,72 @@ if(!OMDB_KEY){
 
 app.use(cors());
 
-app.get("/search",async (req, res) => {
+// Search for movies by title
+app.get("/movies/search",async (req, res) => {
   try{
     let topResult = {};
 
     if(req.query.title){
       let title = encodeURIComponent(req.query.title);
-      resp = await axios.get(`${OMDB_URL}/?s=${title}&apikey=${OMDB_KEY}`);
+      resp = await axios.get(`${OMDB_URL}/?s=${title}&r=json&apikey=${OMDB_KEY}`);
       topResult = resp.data.Search[0] || {};
+      console.log(topResult);
     }
-    console.log(`Searched for "${req.query.title}" - found "${topResult.Title}"`);
 
+    console.log(`Searched for "${req.query.title}" -> found "${topResult.Title}"`);
     res.send(topResult);
+
   } catch(err) {
     res.send({});
-    console.warn(`Problem fetching results`)
+    console.warn(`Problem searching for "${req.query.title}"`)
     console.warn(err);
   }
 });
+
+// Fetch movie by IMDB id
+app.get("/movie/:id", async (req, res) => {
+  try{
+    let movie = {};
+
+    if(req.params.id){
+      let id = encodeURIComponent(req.params.id);
+      resp = await axios.get(`${OMDB_URL}/?i=${id}&r=json&apikey=${OMDB_KEY}`);
+      console.log('RESP: '+ resp)
+      movie = resp.data || {};
+    }
+
+    console.log(`Fetched movie by id: ${req.params.id} -> found "${movie.Title}"`);
+    res.send(movie);
+
+  } catch(err) {
+    console.warn(`Problem fetching movie by id: ${req.params.id}`)
+    console.warn(err);
+    res.send({});
+  }
+});
+
+// Fetch movie by title
+app.get("/movie/byTitle", async (req, res) => {
+  try{
+    let movie = {};
+
+    if(req.query.title){
+      let title = encodeURIComponent(req.query.title);
+      resp = await axios.get(`${OMDB_URL}/?t=${title}&r=json&apikey=${OMDB_KEY}`);
+      movie = resp.data || {};
+    }
+
+    console.log(`Fetched movie by title: "${req.query.title}" -> found "${movie.Title}"`);
+    res.send(topResult);
+
+  } catch(err) {
+    res.send({});
+    console.warn(`Problem fetching movie by title: "${req.query.title}"`)
+    console.warn(err);
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Movie Finder API Server started on port ${PORT}`);
 })
