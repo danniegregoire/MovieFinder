9fd934045223c7c005bfa8826f979b7b4317f53c
Dannie Gregoire <dannie@dgregoire.com>
2021-02-13 20:24:48 -0500

    styled movie result

--- client/src/components/Movie/Movie.js
+++ client/src/components/Movie/Movie.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Movie.css';
+import Tomato from './tomato.svg';
 
 const Movie = ({movie}) => {
+  let ratings = {};
+
+  if(movie.Ratings){
+    movie.Ratings.map(rating => ratings[rating.Source]= rating.Value);
+  }
+
   return (
-    <div>
-      Title: {movie.Title}
-      Year:
-      Rating:
-      Released:
-      Runtime:
-      Awards:
-      Staring:
-      
-      <img className="poster" src={movie.Poster}/>
+    <div className='movie-result'>
+      <img className='movie-poster' src={movie.Poster} alt='{movie.Title} movie poster'/>
+      <h1>{movie.Title}</h1>
+      <span className='movie-year'>{movie.Year}</span>&bull;<span className='movie-rating'>{movie.Rated}</span>&bull;<span className='movie-runtime'>{movie.Runtime}</span>&bull;
+        {ratings['Rotten Tomatoes'] !== undefined ? (
+            <span className='movie-review'><img src={Tomato} alt="Rotten Tomatoes"/> {ratings['Rotten Tomatoes']}</span>
+          ) : (
+            <span style={{fontSize: '.8em'}}>(no reviews)</span>
+        )}
+      <p>{movie.Plot}</p>
     </div>
   );
 }
